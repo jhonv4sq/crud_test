f1aee228bd979cd51142828d69877c25f95c5286
Jhon Erick <jhonv4sq.01@gmail.com>
2024-08-28 01:17:28 +0200

    add view index and create

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'blogs/index.html', {'posts': posts})
+
+def create(request):
+    return render(request, 'blogs/create.html')
+    
+# def store(request):
+#     return ''
+
+# def edit(request):
+#     return ''
+
+# def update(request):
+#     return ''
+
+# def destroy(request):
+#     return ''
