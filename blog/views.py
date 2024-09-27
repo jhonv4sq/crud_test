c115bcaa119791ac581bcdbab29dc6b97c006e55
Jhon Erick <jhonv4sq.01@gmail.com>
2024-09-27 14:55:57 +0200

    add context

--- blog/views.py
+++ blog/views.py
@@ -8,14 +8,32 @@ def index(request):
     posts = Post.objects.all()
     return render(request, 'blogs/index.html', {'posts': posts})
 
+def show(request, id):
+    post = get_object_or_404(Post, id=id)
+    
+    context = {
+        'post': post
+    }
+
+    return render(request, 'blogs/show.html', context)
+
 @login_required
 def create(request):
     form = PostForm()
-    return render(request, 'blogs/create.html', {'form': form})
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'blogs/create.html', context)
     
 @login_required
 def store(request):
     form = PostForm(request.POST)
+    
+    context = {
+        'form': form
+    }
     
     if form.is_valid():
         post = form.save(commit=False)  # Crea una instancia del modelo Post pero no la guarda en la base de datos
@@ -23,11 +41,7 @@ def store(request):
         post.save()  # Ahora guarda el post en la base de datos, con el autor asignado
         return redirect('posts')
     else:
-        return render(request, 'blogs/create.html', {'form': form})
-    
-def show(request, id):
-    post = get_object_or_404(Post, id=id)
-    return render(request, 'blogs/show.html', {'post': post})
+        return render(request, 'blogs/create.html', context)
 
 @login_required
 def destroy(request, id):
@@ -39,15 +53,26 @@ def destroy(request, id):
 def edit(request, id):
     post = get_object_or_404(Post, id=id)
     form = PostForm(instance=post)
-    return render(request, 'blogs/edit.html', {'form': form, 'post': post})
+    
+    context = {
+        'form': form,
+        'post': post
+    }
+    
+    return render(request, 'blogs/edit.html', context)
 
 @login_required
 def update(request, id):
     post = get_object_or_404(Post, id=id)
     form = PostForm(request.POST, instance=post)
     
+    context = {
+        'form': form,
+        'post': post
+    }
+    
     if form.is_valid():
         form.save()
         return redirect('posts')
     else:
-        return render(request, 'blogs/edit.html', {'form': form, 'post': post})
+        return render(request, 'blogs/edit.html', context)
